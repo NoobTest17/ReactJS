21daf007582a1ea29c40f5b1edbeb529a12f6b02
NoobTest17 <maximnnn.nnn@yandex.ru>
2023-01-09 21:40:37 +0300

    Init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import React from "react";
+import './App.css';
+import Header from "./componets/Header";
+import Navbar from "./componets/Nav";
+import Profile from "./componets/Profile";
+
+const App = () => {
+  return (
+    <div className='app-wrapper'>
+      <Header/>
+      <Navbar/>
+      <Profile/>
+    </div>
+  )
+}
+
+export default App;
